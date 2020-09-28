95a440c0eb95d65172299d37070439aac35fbc7b
Bhagi <puppyreddy1997@gmail.com>
2020-09-28 14:39:12 +0530

    [Bhagyalaxmi]Added:UC2_Gambler win or loss to bet

--- GamblerSimulation.java
+++ GamblerSimulation.java
@@ -1,12 +1,31 @@
 import java.util.*;
 class GamblerSimulation
 {
-   static final int STAKE=100;
-   static final int BET=1;
+   static int STAKE=100;
+   static int BET=1;
+   static int Won=0;
+   static int Loss=0;
+   int value;
+
+         public int GamblerGame()
+        {
+          int BettingToss=(int)Math.floor(Math.random()*10)%2;
+            if(BettingToss == 1)
+            {
+             STAKE=STAKE+BET;
+            }
+            else
+            {
+             STAKE=STAKE-BET;
+            }
+    return value;
+         }
 
 
         public static void main(String args[])
        {
           System.out.println("Welcome to GamblerSimulation");   
+            GamblerSimulation played=new GamblerSimulation();
+              played.GamblerGame();
         }
 }
