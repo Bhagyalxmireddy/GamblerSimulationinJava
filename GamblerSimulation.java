d679e43c6ecc04e23c8ef85f736df45eb4682b6b
Bhagi <puppyreddy1997@gmail.com>
2020-09-29 14:28:47 +0530

    [Bhagyalaxmi]Refactered:Modifying in print statement

--- GamblerSimulation.java
+++ GamblerSimulation.java
@@ -5,8 +5,7 @@ class GamblerSimulation
    static final int BET = 1;
 
 
-        public static void main(String args[])
-       {
+        public static void main(String args[]){
           System.out.println("Welcome to GamblerSimulation");   
         }
 }
