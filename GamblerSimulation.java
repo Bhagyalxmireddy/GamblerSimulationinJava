c868493126380b3674454910c075286425b5e287
Bhagi <puppyreddy1997@gmail.com>
2020-09-29 15:11:49 +0530

    [Bhagyalaxmi]Refactered:calcualting the percentage of 50%

--- GamblerSimulation.java
+++ GamblerSimulation.java
@@ -1,44 +1,25 @@
 import java.util.*;
 class GamblerSimulation
 {
-<<<<<<< HEAD
-   static int STAKE = 100;
-   static int BET = 1;
-   static int Won = 0;
-   static int Loss = 0;
-   static int PERCENT = 50;
-   int Result;
-   int Winning = 0;
-   int Lossing = 0;
-
-         public int gamblerGame()
-        {
-          int VALUE=STAKE * PERCENT;
-           VALUE=VALUE / 100;
-           Winning=STAKE + VALUE;
-           System.out.println("The Winning value is:" +Winning);
-           Lossing=STAKE - VALUE;
-           System.out.println("The Lossing value is:" +Lossing);
-          int BettingToss=(int)Math.floor(Math.random()*10)%2;
-            if(BettingToss == 1)
-            {
-             STAKE=STAKE+BET;
-            }
-            else
-            {
-             STAKE=STAKE-BET;
-            }
-    return Result;
-=======
 	static final int STAKE = 100;
-	static final int BET = 1;
-	static int Won = 0;
-   	static int Loss = 0;
-   	int value;
-   	int Stake;
+   	static final int BET = 1;
+   	static int Won = 0;
+        static int Loss = 0;
+        int value;
+        int Stake;
+	static int PERCENT = 50;
+        int Result;
+        int Winning = 0;
+        int Lossing = 0;
 
          public int gamblerGame()
-         {
+          {
+          	int VALUE=STAKE * PERCENT;
+           	VALUE=VALUE / 100;
+           	Winning=STAKE + VALUE;
+           	System.out.println("The Winning value is:" +Winning);
+           	Lossing=STAKE - VALUE;
+           	System.out.println("The Lossing value is:" +Lossing);
           	int bettingToss = (int)Math.floor(Math.random()*10)%2;
             	if(bettingToss == 1)
             	{
@@ -51,7 +32,6 @@ class GamblerSimulation
             	   System.out.println(" gambler loss " +Stake);
             	}
     		return value;
->>>>>>> e4a3c638d5cb1c6cd0cde97975f72e7bb020c30d
          }
 
 
