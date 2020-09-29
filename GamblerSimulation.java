fdbb72b83908d47f0f200e0b4f588854c67301a3
Bhagi <puppyreddy1997@gmail.com>
2020-09-29 08:12:35 +0530

    [Bhagyalaxmi]Refactered:printing the value

--- GamblerSimulation.java
+++ GamblerSimulation.java
@@ -1,22 +1,24 @@
 import java.util.*;
 class GamblerSimulation
 {
-   static int STAKE=100;
-   static int BET=1;
-   static int Won=0;
-   static int Loss=0;
+   static int STAKE = 100;
+   static int BET = 1;
+   static int Won = 0;
+   static int Loss = 0;
    int value;
 
-         public int GamblerGame()
+         public int gamblerGame()
         {
           int BettingToss=(int)Math.floor(Math.random()*10)%2;
             if(BettingToss == 1)
             {
              STAKE=STAKE+BET;
+            System.out.println(" gamler won " +STAKE);
             }
             else
             {
              STAKE=STAKE-BET;
+            System.out.println(" gambler loss " +STAKE);
             }
     return value;
          }
@@ -26,6 +28,6 @@ class GamblerSimulation
        {
           System.out.println("Welcome to GamblerSimulation");   
             GamblerSimulation played=new GamblerSimulation();
-              played.GamblerGame();
+              played.gamblerGame();
         }
 }
