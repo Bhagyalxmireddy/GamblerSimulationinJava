9cf303ba095c4cd8c34e86b09cf40e5d23626a7f
Bhagi <puppyreddy1997@gmail.com>
2020-10-01 10:41:18 +0530

    [Bhagyalaxmi]Refactered:following the naming covestion

--- GamblerSimulation.java
+++ GamblerSimulation.java
@@ -3,31 +3,30 @@ class GamblerSimulation
 {
 	static final int STAKE = 100;
 	static final int BET = 1;
-	static int Won = 0;
-   	static int Loss = 0;
+	static int won = 0;
+   	static int loss = 0;
    	int value;
-   	int Stake;
+   	int stake;
 
          public int gamblerGame()
          {
           	int bettingToss = (int)Math.floor(Math.random()*10)%2;
             	if(bettingToss == 1)
             	{
-                   Stake = STAKE + BET;
-                   System.out.println(" gamler won " +Stake);
+                   stake = STAKE + BET;
+                   System.out.println(" gamler won " +stake);
             	}
                 else
                 {
-                   Stake = STAKE - BET;
-            	   System.out.println(" gambler loss " +Stake);
+                   stake = STAKE - BET;
+            	   System.out.println(" gambler loss " +stake);
             	}
     		return value;
-         }
-
-
-        public static void main(String args[]){
-          System.out.println("Welcome to GamblerSimulation");   
-            GamblerSimulation played=new GamblerSimulation();
-              played.gamblerGame();
+        }
+	public static void main(String args[])
+	{
+        	System.out.println("Welcome to GamblerSimulation");   
+            	GamblerSimulation played=new GamblerSimulation();
+              	played.gamblerGame();
         }
 }
